ac304408985d29653d59400d8ab6d3c0cecfca99
DrowsyLad <tsumwashere@student.ub.ac.id>
2023-01-15 22:07:35 +0700

    Add ROBOTIIK logo to as turtle in turtlesim

--- turtlesim/src/turtle_frame.cpp
+++ turtlesim/src/turtle_frame.cpp
@@ -75,20 +75,21 @@ TurtleFrame::TurtleFrame(QWidget* parent, Qt::WindowFlags f)
   }
 
   QVector<QString> turtles;
-  turtles.append("box-turtle.png");
-  turtles.append("robot-turtle.png");
-  turtles.append("sea-turtle.png");
-  turtles.append("diamondback.png");
-  turtles.append("electric.png");
-  turtles.append("fuerte.png");
-  turtles.append("groovy.png");
-  turtles.append("hydro.svg");
-  turtles.append("indigo.svg");
-  turtles.append("jade.png");
-  turtles.append("kinetic.png");
-  turtles.append("lunar.png");
-  turtles.append("melodic.png");
-  turtles.append("noetic.png");
+  // turtles.append("box-turtle.png");
+  // turtles.append("robot-turtle.png");
+  // turtles.append("sea-turtle.png");
+  // turtles.append("diamondback.png");
+  // turtles.append("electric.png");
+  // turtles.append("fuerte.png");
+  // turtles.append("groovy.png");
+  // turtles.append("hydro.svg");
+  // turtles.append("indigo.svg");
+  // turtles.append("jade.png");
+  // turtles.append("kinetic.png");
+  // turtles.append("lunar.png");
+  // turtles.append("melodic.png");
+  // turtles.append("noetic.png");
+  turtles.append("robotiik-robot.png");
 
   QString images_path = (ros::package::getPath("turtlesim") + "/images/").c_str();
   for (int i = 0; i < turtles.size(); ++i)
